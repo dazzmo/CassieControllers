07b04e0243d5276eb180422e45752de57e2f9b69
dazzmo <dabo9333@uni.sydney.edu.au>
2024-02-29 12:46:36 +1100

    Minor work towards angle-tracking, need to consider how this will affect the constraint handling in the QP (i.e. only translational forces should be considered)

--- controllers/include/controllers/osc/tasks/end_effector_task.h
+++ controllers/include/controllers/osc/tasks/end_effector_task.h
@@ -8,9 +8,20 @@ namespace osc {
 
 class EndEffectorTask : public Task {
    public:
+    // Tracking types
+    enum class TrackingType {
+        kTranslation = 0,
+        kRotation,
+        kTranslationAndRotation
+    };
+
     bool inContact = false;
 
-    EndEffectorTask(const std::string& name, const DynamicModel::Size& sz, Task::TaskCallbackFunction& callback);
+    EndEffectorTask(
+        const std::string& name, const DynamicModel::Size& sz,
+        Task::TaskCallbackFunction& callback,
+        const TrackingType tracking_type = TrackingType::kTranslation);
+        
     ~EndEffectorTask() = default;
 
     void SetId(const int& id) { id_ = id; }
@@ -25,12 +36,24 @@ class EndEffectorTask : public Task {
     Vector3& normal() { return normal_; }
     const Vector3& normal() const { return normal_; }
 
+    /**
+     * @brief Custom update method to address the different error metric that
+     * comes with rotation groups (i.e. using the logarithm map to map to the
+     * Lie algebra of the rotation group)
+     *
+     * @param q Current configuration of the sytem
+     * @param v Current velocity of the system
+     */
+    void Update(const ConfigurationVector& q, const TangentVector& v) override;
+
    protected:
    private:
     int id_;
     Scalar mu_;
     Vector3 lambda_;
     Vector3 normal_;
+
+    TrackingType track_type_;
 };
 
 }  // namespace osc
